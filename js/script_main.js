ddbbd78e0ebe59c87e0d466f42c99c04e04b6d91
Renzo <RenBTL350@gmail.com>
2021-07-05 23:55:48 -0500

    4to Update

--- js/script_main.js
+++ js/script_main.js
@@ -13,3 +13,13 @@ $("#op1").click(function (e) {
     e.preventDefault();
     window.location = 'funciones/1_crear_usuario.html';
 });
+
+$("#op2").click(function (e) { 
+    e.preventDefault();
+    window.location = 'funciones/2_crear_capacit.html';
+});
+
+$("#op3").click(function (e) { 
+    e.preventDefault();
+    window.location = 'funciones/3_control_capacit.html';
+});
